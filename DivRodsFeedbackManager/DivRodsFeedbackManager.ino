9dac372fa6b5c9cca9849abec50572b1a803b9a6
Maxwell Hoaglund <maxwell.hoaglund@gmail.com>
2017-05-17 13:47:11 -0500

    updated feedback client firmware

--- DivRodsFeedbackManager/DivRodsFeedbackManager.ino
+++ DivRodsFeedbackManager/DivRodsFeedbackManager.ino
@@ -21,27 +21,38 @@
 //Target is Arduino 328.
 #define RING_DATA_PIN 6
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(8, RING_DATA_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(12, RING_DATA_PIN, NEO_GRB + NEO_KHZ800);
 Adafruit_BNO055 bno = Adafruit_BNO055(55);
 
-char buf[100];
-volatile byte pos;
-volatile bool process_it;
+const byte numChars = 32;
+char receivedChars[numChars];
+
+const char headingflag = 'h';
+const char waitflag = 'w';
+const char successflag = 's';
+char _mode = 'h';
+
+boolean newData = false;
 
 int CURRENT_HEADING = 0;
 long previousMillis = 0;
 const int heading_interval = 75;
 byte heading_offset_index = 0;
-byte LED_MAP[][8] = {
-    {0,1,2,3,4,5,6,7},
-    {7,0,1,2,3,4,5,6},
-    {6,7,0,1,2,3,4,5},
-    {5,6,7,0,1,2,3,4},
-    {4,5,6,7,0,1,2,3},
-    {3,4,5,6,7,0,1,2},
-    {2,3,4,5,6,7,0,1},
-    {1,2,3,4,5,6,7,0}
-  };
+
+ byte LED_MAP[][12] = {
+  {0,1,2,3,4,5,6,7,8,9,10,11},
+  {11,0,1,2,3,4,5,6,7,8,9,10},
+  {10,11,0,1,2,3,4,5,6,7,8,9},
+  {9,10,11,0,1,2,3,4,5,6,7,8},
+  {8,9,10,11,0,1,2,3,4,5,6,7},
+  {7,8,9,10,11,0,1,2,3,4,5,6},
+  {6,7,8,9,10,11,0,1,2,3,4,5},
+  {5,6,7,8,9,10,11,0,1,2,3,4},
+  {4,5,6,7,8,9,10,11,0,1,2,3},
+  {3,4,5,6,7,8,9,10,11,0,1,2},
+  {2,3,4,5,6,7,8,9,10,11,0,1},
+  {1,2,3,4,5,6,7,8,9,10,11,0}
+ };
 
 void setup() {
   Serial.begin(9600);
@@ -51,70 +62,98 @@ void setup() {
     Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
     while(1);
   }
-  delay(1000);
+  delay(250);
     
   bno.setExtCrystalUse(true);
   strip.begin();
-  strip.setBrightness(30); //adjust brightness here
+  strip.setBrightness(150); //adjust brightness here
   strip.show(); // Initialize all pixels to 'off'
-  
-  SPCR |= bit (SPE); //somehow this turns on SPI in slave mode. TODO understand
-  pinMode(MISO, OUTPUT);
-  pos = 0;
-  process_it = false;
-  SPI.attachInterrupt();
 }
 
-// SPI interrupt routine. Asynchronously add a received byte to the pile.
-ISR (SPI_STC_vect)
-{
-byte c = SPDR;  // grab byte from SPI Data Register
-  if (pos < sizeof buf){
-    buf [pos++] = c;
-    if (c == '\n') process_it = true;
-   } 
-}  // end of interrupt routine SPI_STC_vect
-int RXbuf = 0;
+int targetled = 0;
+long fadecounter = 0;
+long fadeinterval = 1500;
+bool fadedirection = true;
 void loop() {
   unsigned long currentMillis = millis();
-  if (Serial.available() > 0) {
-      RXbuf = Serial.parseInt();
-      if(RXbuf < 8 & RXbuf > 0){
-        heading_offset_index = RXbuf;
-      }
-      Serial.print("I received: ");
-      Serial.println(RXbuf);
-  }
-  if (process_it){
-      //Should be getting a command byte and a data byte.
-      buf [pos] = 0;
-      Serial.println (buf);
-      if(buf[0]==0) heading_offset_index = buf[1]; //something like that!
-      pos = 0;
-      process_it = false;
-  }
+  recvWithStartEndMarkers();
+  parseSerialPacket();
   if(currentMillis - previousMillis > heading_interval){
     previousMillis = currentMillis;
     sensors_event_t event; 
     bno.getEvent(&event);
-
     int headingint = (int)event.orientation.x;
-    int targetled = mapheadingtoLED(headingint, LED_MAP[heading_offset_index]);
-    strip.setPixelColor(targetled, strip.Color(255, 0, 255));
-    instantColorWipe(strip.Color(0, 125, 155), targetled);
+    targetled = mapheadingtoLED(headingint, LED_MAP[heading_offset_index]);
+    int attitude = (int)event.orientation.y;
+    if(attitude > 45){
+      strip.setBrightness(15);
+    }else{
+      strip.setBrightness(150);
+    }
+  }
+  
+  switch (_mode) {
+    case 'h':{
+      strip.setPixelColor(targetled, strip.Color(255, 0, 255));
+      instantColorWipe(strip.Color(0, 125, 155), targetled);
+      break;
+    }
+    case 'w':{
+      if(fadedirection) fadecounter++;
+      if(!fadedirection) fadecounter--;
+      if(fadecounter > fadeinterval) fadedirection = false;
+      if(fadecounter < 1) fadedirection = true;
+      int rchan = map(fadecounter, 0, fadeinterval, 2, 145);
+      int bchan = map(fadecounter, 0, fadeinterval, 255, 25);
+      fullColorWipe(strip.Color(rchan, bchan, 125));
+      break;
+    }
+    case 's':{
+      if(fadedirection) fadecounter += 5;
+      if(!fadedirection) fadecounter -= 5;
+      if(fadecounter > fadeinterval) fadedirection = false;
+      if(fadecounter < 1) fadedirection = true;
+      int gchan = map(fadecounter, 0, fadeinterval, 25, 255);
+      int bchan = map(fadecounter, 0, fadeinterval, 255, 25);
+      fullColorWipe(strip.Color(0, gchan, bchan));
+      break;
+    }
+    default: 
+    break;
   }
 }
 
 uint32_t mapheadingtoLED(int heading, byte indices[]){
   uint32_t led = 0;
-  if(heading >= 0 & heading < 45) led = indices[7];
-  if(heading >= 45 & heading < 90) led = indices[6];
-  if(heading >= 90 & heading < 135) led = indices[5];
-  if(heading >= 135 & heading < 180) led = indices[4];
-  if(heading >= 180 & heading < 225) led = indices[3];
-  if(heading >= 225 & heading < 270) led = indices[2];
-  if(heading >= 270 & heading < 315) led = indices[1];
-  if(heading >= 315 & heading < 360) led = indices[0];
+  if(heading >= 0 & heading < 30) led = indices[11];
+  if(heading >= 30 & heading < 60) led = indices[10];
+  if(heading >= 60 & heading < 90) led = indices[9];
+  if(heading >= 90 & heading < 120) led = indices[8];
+  if(heading >= 120 & heading < 150) led = indices[7];
+  if(heading >= 150 & heading < 180) led = indices[6];
+  if(heading >= 180 & heading < 210) led = indices[5];
+  if(heading >= 210 & heading < 240) led = indices[4];
+  if(heading >= 240 & heading < 270) led = indices[3];
+  if(heading >= 270 & heading < 300) led = indices[2];
+  if(heading >= 300 & heading < 330) led = indices[1];
+  if(heading >= 330 & heading < 360) led = indices[0];
+  return led;
+}
+
+uint32_t roundHeading(int heading){
+  uint32_t led = 0;
+  if(heading >= 0 & heading < 30) led = 11;
+  if(heading >= 30 & heading < 60) led = 10;
+  if(heading >= 60 & heading < 90) led = 9;
+  if(heading >= 90 & heading < 120) led = 8;
+  if(heading >= 120 & heading < 150) led = 7;
+  if(heading >= 150 & heading < 180) led = 6;
+  if(heading >= 180 & heading < 210) led = 5;
+  if(heading >= 210 & heading < 240) led = 4;
+  if(heading >= 240 & heading < 270) led = 3;
+  if(heading >= 270 & heading < 300) led = 2;
+  if(heading >= 300 & heading < 330) led = 1;
+  if(heading >= 330 & heading < 360) led = 0;
   return led;
 }
 
@@ -127,3 +166,67 @@ void instantColorWipe(uint32_t c, int remnant){
   }
   strip.show();
 }
+
+//Set color of all but passed in pixel
+void fullColorWipe(uint32_t c){
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, c);  
+  }
+  strip.show();
+}
+
+void recvWithStartEndMarkers() {
+    static boolean recvInProgress = false;
+    static byte ndx = 0;
+    char startMarker = '<';
+    char endMarker = '>';
+    char rc;
+ 
+    while (Serial.available() > 0 && newData == false) {
+        rc = Serial.read();
+
+        if (recvInProgress == true) {
+            if (rc != endMarker) {
+                receivedChars[ndx] = rc;
+                ndx++;
+                if (ndx >= numChars) {
+                    ndx = numChars - 1;
+                }
+            }
+            else {
+                receivedChars[ndx] = '\0'; // terminate the string
+                recvInProgress = false;
+                ndx = 0;
+                newData = true;
+            }
+        }
+
+        else if (rc == startMarker) {
+            recvInProgress = true;
+        }
+    }
+}
+
+void parseSerialPacket() {
+    if (newData == true) {
+        Serial.print("This just in ... ");
+        Serial.println(receivedChars);
+        newData = false;
+        String temp(receivedChars);
+        if(receivedChars[0] == headingflag){
+          _mode = 'h';
+          Serial.println(temp);
+          temp.remove(0,1);
+          heading_offset_index = roundHeading(temp.toInt());
+          Serial.print("Processed heading: ");
+          Serial.println(heading_offset_index);
+        }
+        else if(receivedChars[0] == waitflag){
+          _mode = 'w';
+        }
+        else if(receivedChars[0] == successflag){
+          _mode = 's';
+        }
+    }
+}
+
